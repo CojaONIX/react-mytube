9ed3919d4bc9f9b5e10498abafffa00a2f90105d
CojaONIX <cojaonix@gmail.com>
2024-04-17 19:12:05 +0200

    5.4 Videos list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 
 
+const VIDEOS = [
+    {
+        id: 1,
+        title: "Realna situacija u IT-u",
+        link: "https://www.youtube.com/watch?v=eV0E2-_8UJg&t=801s",
+        cover: "https://i.ytimg.com/vi/eV0E2-_8UJg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCoK3KQilouChVzsc5yBSh8NP9MOw"
+    },
+    {
+        id: 2,
+        title: "Kako naučiti programiranje?",
+        link: "https://www.youtube.com/watch?v=Dtdr505IuYM",
+        cover: "https://i.ytimg.com/vi/Dtdr505IuYM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA_ToFytfp9PU8DPYz9wfZI1cPHFQ"
+    }
+]
 const App = () => {
   return (
     <div>
