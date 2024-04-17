be39d9f249526370888519fbfceafbcb3cc90712
CojaONIX <cojaonix@gmail.com>
2024-04-17 19:50:42 +0200

    5.5 Videos bootstrap cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const VIDEOS = [
     {
@@ -13,14 +13,41 @@ const VIDEOS = [
         link: "https://www.youtube.com/watch?v=Dtdr505IuYM",
         cover: "https://i.ytimg.com/vi/Dtdr505IuYM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA_ToFytfp9PU8DPYz9wfZI1cPHFQ"
     }
-]
+];
 const App = () => {
-  return (
-    <div>
+    return (
+        <div className="container">
 
-      <h1>Hello</h1>
-    </div>
-  );
+            <h2>MyTube</h2>
+
+            <hr/>
+
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            {
+                VIDEOS.map((video) => {
+                    return  <div className="card-group">
+                                <div className="card">
+
+                                    <div className="card-header">
+                                        <p>id: {video.id}</p>
+                                    </div>
+
+                                    <img src={video.cover} className="card-img-top" alt="..."/>
+
+                                    <div className="card-body">
+                                        <h5>{video.title}</h5>
+                                    </div>
+
+                                    <div className="card-footer d-flex justify-content-between">
+                                        <a href={video.link} className="btn btn-primary" target="_blank">Play</a>
+                                    </div>
+                                </div>
+                            </div>
+                })
+            }
+            </div>
+        </div>
+    );
 }
 
 export default App;
